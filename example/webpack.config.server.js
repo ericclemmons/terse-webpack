d503494d18b3af9df9e27ddcc8eaa4c97914e9b8
Eric Clemmons <eric@smarterspam.com>
2016-07-17 17:01:21 -0500

    Add comment that externals RegExp does work

--- example/webpack.config.server.js
+++ example/webpack.config.server.js
@@ -1,9 +1,6 @@
 var api = require("./webpack.config.defaults")
   .entry("./src/server.js")
-  // @TODO Auto-installed deps aren't in node_modules (yet),
-  // so let's see if this is a problem or not
   .externals(/^@?\w[a-z\-0-9\./]+$/)
-  // .externals(...require("fs").readdirSync("./node_modules"))
   .output("build/server")
   .target("node")
   .when("development", function(api) {
